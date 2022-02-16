965dd244435f38b2823c67713e36d0103a0ce4ea
Ubuntu <ubuntu@ip-172-31-4-155.eu-west-1.compute.internal>
2022-02-16 11:44:19 +0000

    funciones

--- conceptos/4-Programación Procedural/funciones.adb
+++ conceptos/4-Programación Procedural/funciones.adb
@@ -1,5 +1,6 @@
 -- Programación procedural
--- Capacidad de definir bloques de código reutilizables:
+--  Estructurar mejor mi código
+--  Capacidad de definir bloques de código reutilizables:
 -- - Procedimientos: Bloque de código reutilizable, que:
         -- - Cuando lo llamo (solicito su ejecución) puedo sumistrar una serie de argumentos
         -- - El procedimiento ejecuta unas tareas y acaba.
@@ -20,6 +21,43 @@ procedure Funciones is
     begin
         return Numero * 2 ;
     end Doblar;
+    
+    
+    function Mayor (Numero1: Integer; Numero2: Integer) return Integer is
+        EL_MAYOR: Integer;
+    begin
+        -- Forma muy formal y Muy válida
+        -- if Numero1 > Numero2 then
+        --    EL_MAYOR := Numero1;
+        -- else
+        --     EL_MAYOR := Numero2;
+        -- end if;
+        -- return El_MAYOR;
+        
+        -- Forma sencilla
+        --if Numero1 > Numero2 then
+        --    return Numero1;
+        --else
+        --    return Numero2;
+        --end if;
+        
+        -- Forma para que no me duelan los dedo mucho
+        -- if Numero1 > Numero2 then
+        --    return Numero1;
+        -- end if;
+        -- return Numero2;
+        
+        -- Forma guay!
+        return if Numero1 > Numero2 then Numero1 else Numero2;
+    end;
+    
+    
+    function MayorDeTres (Numero1: Integer; Numero2: Integer; Numero3: Integer) return Integer is
+    begin
+        return Mayor( Mayor (Numero1, Numero2), Numero3);
+    end MayorDeTres;
+
+
 begin
     
     Put_line("Voy a calcular el doble de unos números:");
@@ -31,5 +69,11 @@ begin
     Put_Line("Doble del  7 = " &  Integer'Image( Doblar(  7 ) ) );
     Put_Line("Doble del 14 = " & Integer'Image( Doblar( 14 ) ) );
     Put_Line("Doble del 31 = " & Integer'Image( Doblar( 31 ) ) );
+    
+    Put_Line("El mayor entre el 10 y el 20 es: " & Integer'Image( Mayor( 10, 20 ) ) );
+    -- La función debe devolver el mayor de los 2 número que 
+    -- recibe como argumento
+    -- Si son iguales, cualquiera de ellos
+    Put_Line("El mayor entre el 10, el 30 y el 20 es: " & Integer'Image( MayorDeTres( 10, 30, 20 ) ) );
     
 end Funciones;
