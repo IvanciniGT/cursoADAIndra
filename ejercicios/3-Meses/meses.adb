d5a3f30355253561232a270553a13f120bc6311a
Ubuntu <ubuntu@ip-172-31-4-155.eu-west-1.compute.internal>
2022-02-16 10:37:47 +0000

    meses

--- ejercicios/3-Meses/meses.adb
+++ ejercicios/3-Meses/meses.adb
@@ -8,21 +8,29 @@ procedure Meses is
     -- Aqui digo los posibles meses
     type MES is (ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE);
     MESES: array (1..12) of MES;
+    DIAS_DE_CADA_MES: array (MES) of Integer; -- Los propios meses son el indice del array
     MESES_VACACIONES: array (1..3) of MES;
 begin
     -- Aqui digo los meses de un año
     MESES:= (ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE);
     -- Aqui defino los meses de vacaicones
     MESES_VACACIONES:= (JULIO, AGOSTO, SEPTIEMBRE);
-    
+    -- Aqui defino los dias de cada mes
+    DIAS_DE_CADA_MES:= (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+    --DIAS_DE_CADA_MES(ENERO):=31;
+    --DIAS_DE_CADA_MES(FEBRERO):=28;
     Put_Line("Meses del año");
+--    for indice_mes in 1..12 loop
     for mes of MESES loop  
-        Put_Line( MES'Image(mes) );                       
+        --Put_Line( MES'Image(MESES(indice_mes)) & " " &Integer'Image( DIAS_DE_CADA_MES(indice_mes) ) );                       
+        Put_Line( MES'Image(mes) & " " &Integer'Image( DIAS_DE_CADA_MES(mes) ) );                       
     end loop;
     
     Put_Line("Meses de vacaciones");
     for mes of MESES_VACACIONES loop  
-        Put_Line( MES'Image(mes) );                         
+        Put_Line( MES'Image(mes) & " " &Integer'Image( DIAS_DE_CADA_MES(mes) ) );                         
     end loop;
+    
+    Put(DIAS_DE_CADA_MES(FEBRERO));
     
 end Meses;
